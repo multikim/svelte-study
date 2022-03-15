b95815cb5eee9b57aca4cee7ede4bc8734150626
Youngchan Kim <multi0706@gmail.com>
2022-03-15 11:00:05 +0900

    initial commit...

---
+++ 가장빨리만나는스벨트/debugSample/debugSample/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name = "world";
+
+	console.log(`normal debug: ${name}`); // 1. 일반 디버깅 코드
+
+	$:console.log(`reactive debug: ${name}`); // 2. 리액티브 디버깅 코드
+</script>
+
+<input text=text bind:value={name}>
+
+<!-- {@debug name} --> <!-- name 값 확인 -->
+
+<h1>Hello, {name}!</h1>
---
+++ 스벨트실습/using-child/src/CustomSelect.svelte
@@ -0,0 +1,19 @@
+<!-- 스크립트 블록 -->
+<script>
+    const list = [
+        { code: "apple", desc: "사과"},
+        { code: "kiwi", desc: "키위" },
+        { code: "mango", desc: "망고" },
+        { code: "strawberry", desc: "딸기" }
+    ];
+</script>
+<!-- HTML 블록 -->
+<main>
+    <select> <!-- 과일 select 박스 -->
+        <option value="">선택하세요</option>
+        {#each list as item}
+    </select>
+</main>
+<!-- 스타일 블록 -->
+<style>
+</style>
